e3118a97daf51611c93a3a175df768fdcaf76311
Phil Vachon <phil@security-embedded.com>
2021-12-06 07:55:52 -0500

    Day 6

---
+++ day6/six_second.adb
@@ -0,0 +1,81 @@
+with Ada.Text_IO,
+     Ada.Command_Line;
+
+procedure Six_Second is
+    package Text_IO renames Ada.Text_IO;
+    package Command_Line renames Ada.Command_Line;
+
+    file : Text_IO.File_Type;
+
+    type Life_Cycle is range 0..8;
+
+    type Fish_Count is range 0..2**64;
+
+    type States is array(Life_Cycle) of Fish_Count;
+    fish_states : States := (others => 0);
+
+    procedure Load_Initial_States(line : String) is
+        pos : Natural := 1;
+        state : Life_Cycle;
+    begin
+        while pos <= line'Length loop
+            state := Character'Pos(line(pos)) - 48;
+            fish_states(state) := fish_states(state) + 1;
+            pos := pos + 2;
+        end loop;
+    end Load_Initial_States;
+
+    procedure Dump_States is
+        sum : Fish_Count := 0;
+    begin
+        for I in States'Range loop
+            Text_IO.Put(Life_Cycle'Image(I) & ": " & Fish_Count'Image(fish_states(I)) & ", ");
+            sum := sum + fish_states(I);
+        end loop;
+        Text_IO.Put_Line(" --> SUM: " & Fish_Count'Image(sum));
+    end Dump_States;
+begin
+
+    Text_IO.Put_Line("Advent of Code 2021: 6.1");
+
+    if Command_Line.Argument_Count /= 1 then
+        Text_IO.Put_Line("Need to specify initial problem. Aborting.");
+        return;
+    end if;
+
+    Text_IO.Put_Line("File name: " & Command_Line.Argument(1));
+
+    Text_IO.Open(File => file,
+                 Mode => Text_IO.In_File,
+                 Name => Command_Line.Argument(1));
+
+    Load_Initial_States(Text_IO.Get_Line(file));
+
+    Dump_States;
+
+    for I in 1..256 loop
+        declare
+            new_states : States := (others => 0);
+        begin
+            -- If the fish are in state 0, copy them to state 8
+            new_states(8) := fish_states(0);
+
+            -- Now shift the array down
+            new_states(7) := fish_states(8);
+            -- Move the fish that just gave birth back to state 6
+            new_states(6) := fish_states(7) + fish_states(0);
+            new_states(5) := fish_states(6);
+            new_states(4) := fish_states(5);
+            new_states(3) := fish_states(4);
+            new_states(2) := fish_states(3);
+            new_states(1) := fish_states(2);
+            new_states(0) := fish_states(1);
+
+            fish_states := new_states;
+
+            Text_IO.Put("Day: " & Natural'Image(I) & ": ");
+            Dump_States;
+        end;
+    end loop;
+
+end Six_Second;
